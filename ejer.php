ccd19a90e26dec644f2a20a34b8055e7fe7bab6f
ridger <jgjahuar@hotmail.com>
2015-05-06 10:33:11 -0500

    asjdfbej

---
+++ ejer.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+	</head>
+		<body>
+				<form method="get" action="s.php">	
+					<br>
+					Numero A: 
+					<input type="text" name ="a">
+					<p>
+					Numero B: 
+					<input type="text" name ="b">
+					<p>
+					<input type="submit"  name="aceptar" value="Aceptar">
+
+				</form>
+		</body>
+</html>
